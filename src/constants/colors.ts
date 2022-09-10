594533ef64183f1cffe524498f7d29293f324a60
aryamanrishabh <aryamanrishabh@gmail.com>
2022-09-10 19:12:24 +0530

    add constants

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -4,4 +4,4 @@ export const BLACK: string = "#000";
 export const RED: string = "#fa2d2d";
 export const GREEN: string = "#07a907";
 export const BLUE: string = "#1c75dc";
-export const YELLOW: string = "#f5f535";
+export const YELLOW: string = "#ecd407";
