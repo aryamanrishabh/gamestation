c7b4cbaee8d821f19686a9698d04f2b57ef327ce
aryamanrishabh <aryamanrishabh@gmail.com>
2022-11-12 18:51:58 +0530

    redux store setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
-import "@styles/globals.css";
+import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
+import { persistor, store } from "@redux/store";
+import "@styles/globals.css";
+import { PersistGate } from "redux-persist/integration/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="app">
-      <Component {...pageProps} />
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="app">
+          <Component {...pageProps} />
+        </div>
+      </PersistGate>
+    </Provider>
   );
 }
 
---
+++ src/redux/store.ts
@@ -0,0 +1,42 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import { createWrapper, HYDRATE } from "next-redux-wrapper";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import rootReducer from "./slices";
+
+const persistConfig = {
+  key: "gamestation",
+  storage,
+  whiteList: [],
+};
+
+const reducer = (state: any, action: PayloadAction) => {
+  if (action.type === "persist/REHYDRATE" || action.type === HYDRATE) {
+    return { ...state };
+  }
+  return rootReducer(state, action);
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
+export const wrapper = createWrapper(() => store, { debug: false });
