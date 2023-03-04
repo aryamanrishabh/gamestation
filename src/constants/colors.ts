7bc6941ac92e3724f5d6c92979c6054a31b4218f
Aryaman Rishabh <aryamanrishabh@gmail.com>
2023-03-04 19:55:08 +0530

    update root styles

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -5,3 +5,8 @@ export const RED: string = "#fa2d2d";
 export const GREEN: string = "#07a907";
 export const BLUE: string = "#1c75dc";
 export const YELLOW: string = "#ecd407";
+
+export const PRIMARY = "#537FE7";
+export const SECONDARY = "#C0EEF2";
+export const TERTIARY = "#E9F8F9";
+export const ACCENT = "#181823";
